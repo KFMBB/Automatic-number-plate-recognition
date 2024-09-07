b3a9d792ef847cc43ff16f4829f74a82f6cbc2fa
KFMBB <140276664+KFMBB@users.noreply.github.com>
2024-09-07 11:54:04 +0300

    Update ANPR_main.py

--- ANPR_main.py
+++ ANPR_main.py
@@ -12,13 +12,11 @@ results = {}  # We'll save our results for processing later.
 # load models
 # We're using two pre-trained models:
 # 1- The first model will focus on detecting cars while the other will focus on detecting license plates:
-model = YOLO('yolov8n.pt')  # Load the nano version of the yolo model.
+model = YOLO('models/yolov8n.pt')  # Load the nano version of the yolo model.
 # 2- The second model will be used to detect the license plates.
-# license_plate_detector = YOLO('Plate_detector_Model/plate_dedector.pt')
-license_plate_detector = YOLO('Plate_detector_Model/license_plate_detector.pt')
+license_plate_detector = YOLO('models/best_license_plate_detector.pt')
 # load video
-# cap = cv2.VideoCapture('Readings/License_Plate_detection.mp4')
-cap = cv2.VideoCapture("Readings/License_Plate_detection.mp4") # try a smaller vid
+cap = cv2.VideoCapture("input/testing.mp4")
 # Since we're detecting vehicles we'll set a list with the YOLO class_id for vehicles.
 # why? well to make our model less computationally expensive and make it focus on the task at hand
 vehicles = [2, 3, 5, 7]
@@ -75,4 +73,4 @@ while isReadingFrames:
                                                                   'text_score': license_plate_text_score}}
 
 # write results
-write_csv(results, 'Readings/Results.csv')
+write_csv(results, 'output/results.csv')
